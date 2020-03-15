2147e939b2d81c0323f3fdd359205c6335529233
Hyunjun <Hyunjun@121.137.114.36>
2020-03-16 01:49:59 +0900

    Create default method

--- HousePlugin/src/com/marquez/houseplugin/HousePlugin.java
+++ HousePlugin/src/com/marquez/houseplugin/HousePlugin.java
@@ -3,5 +3,18 @@ package com.marquez.houseplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HousePlugin extends JavaPlugin{
+	
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+	
 
 }
